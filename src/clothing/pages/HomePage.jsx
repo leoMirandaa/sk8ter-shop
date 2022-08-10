c8628a9cb944f3d9dc8a299563182974b521ca0b
Victoria Warren <93948407+vmw19@users.noreply.github.com>
2022-08-09 20:56:29 -0400

    Final Project Images

--- src/clothing/pages/HomePage.jsx
+++ src/clothing/pages/HomePage.jsx
@@ -1,9 +1,15 @@
 
+import CarouselStyleAssessment from "../components/carousel"
+ 
 
 export const HomePage = () => {
   return (
     <>
       <h1>HOME</h1>
+      
+  <CarouselStyleAssessment/> 
+
     </>
+
   )
 }
