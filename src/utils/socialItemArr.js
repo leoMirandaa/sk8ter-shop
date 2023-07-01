2e400446b90454b7d412d1a91b2bcea29c90c57e
leo <leopoldo.miranda18@tectijuana.edu.mx>
2023-07-01 13:32:22 -0700

    feat: add authLayout.scss and login.scss

---
+++ src/utils/socialItemArr.js
@@ -0,0 +1,19 @@
+export const socialItems = [
+  {
+    name: "facebook",
+    icon: "pi pi-facebook",
+    link: "",
+  },
+  {
+    name: "instagram",
+    icon: "pi pi-instagram",
+  },
+  {
+    name: "twitter",
+    icon: "pi pi-twitter",
+  },
+  {
+    name: "youtube",
+    icon: "pi pi-youtube",
+  },
+];
