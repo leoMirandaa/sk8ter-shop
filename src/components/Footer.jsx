7ac6069b5f1b4ecabc2e66a34c50203e20570c78
leo <leopoldo.miranda18@tectijuana.edu.mx>
2023-07-02 13:58:45 -0700

    style: visual identation

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,33 +5,39 @@ import { socialItems } from "../utils/socialItemArr";
 import "../styles/footer.scss";
 export const Footer = () => {
   return (
-    <footer className="footer ">
-      <div className="footer__mailSection ">
-        <h2>Sign up for our mailing list</h2>
-        <p>Be the first to hear about new product</p>
+    <footer className="footer">
+      <div className="footer__mailSection">
+        <div className="container">
+          <h2 className="footer__mailSection__title">
+            Sign up for our mailing list
+          </h2>
+          <p>Be the first to hear about new product</p>
 
-        <div className="footer__mailSection__search">
-          <InputText placeholder="Your Email" />
-          <Button severity="secondary">SIGN UP</Button>
-        </div>
+          <div className="footer__mailSection__search">
+            <InputText placeholder="Your Email" />
+            <Button severity="secondary">SIGN UP</Button>
+          </div>
 
-        <div className="footer__mailSection__followSection">
-          <h2>Follow Us:</h2>
+          <div className="footer__mailSection__followSection">
+            <h2 className="footer__mailSection__followSection__title">
+              Follow Us:
+            </h2>
 
-          <span className="footer__mailSection__followSection__icons">
-            {socialItems.map((item) => (
-              <Button
-                key={item.name}
-                size="small"
-                icon={item.icon}
-                className="bg-white text-primary"
-              />
-            ))}
-          </span>
+            <span className="footer__mailSection__followSection__icons">
+              {socialItems.map((item) => (
+                <Button
+                  key={item.name}
+                  size="small"
+                  icon={item.icon}
+                  className="bg-white text-primary"
+                />
+              ))}
+            </span>
+          </div>
         </div>
       </div>
 
-      <div className="footer__links">
+      <div className="footer__links container">
         <div className="footer__links__container">
           <div className="footer__links__categories">
             <h2>Categories</h2>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -8,7 +8,7 @@ export const HomePage = () => {
   return (
     <div className="animate__animated animate__fadeIn">
       <header className="header">
-        <div className="header__heroImage container">
+        <div className="header__heroImage">
           <div className="header__heroImage__textContainer">
             <h2 className="header__heroImage__textContainer__title">
               20 OFF EVERY THING
@@ -22,28 +22,27 @@ export const HomePage = () => {
         </div>
       </header>
 
-      <div className="container">
-        <section
-          className="discounts"
-          // style={{ background: "black", color: "white" }}
-        >
-          <div>20% OFF</div>
-          <div>20% OFF</div>
-          <div>20% OFF</div>
-        </section>
+      <section
+        className="discounts"
+        // style={{ background: "black", color: "white" }}
+      >
+        <div>20% OFF</div>
+        <div>20% OFF</div>
+        <div>20% OFF</div>
+      </section>
 
-        <section className="quote">
-          <h2 className="quote__author">John Galliano</h2>
-          <p className="quote__phrase">"The joy of dressing is an art." </p>
-        </section>
+      <section className="quote">
+        <h2 className="quote__author">John Galliano</h2>
+        <p className="quote__phrase">"The joy of dressing is an art." </p>
+      </section>
 
-        <section className="categories">
+      <main className="categories">
+        <div className="container">
           <h2 className="categories__title">Categories</h2>
           <div className="categories__container">
             {categories.map((category) => (
               <div
                 key={category.title}
-                // style={{ margin: "0 auto" }}
                 className="categories__container__card"
               >
                 <CategoryCard
@@ -54,8 +53,8 @@ export const HomePage = () => {
               </div>
             ))}
           </div>
-        </section>
-      </div>
+        </div>
+      </main>
     </div>
   );
 };
