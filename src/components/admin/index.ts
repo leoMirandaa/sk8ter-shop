74cdc4a7998349368a6139fb3f939491b486e03c
leo <leopoldo.miranda18@tectijuana.edu.mx>
2023-08-03 23:57:57 -0700

    fix: add layout component folder

---
+++ src/components/admin/index.ts
@@ -0,0 +1,5 @@
+export * from "./MenuCard";
+export * from "./Table";
+export * from "./TableHeader";
+export * from "./TableSkeleton";
+export * from "./UserForm";
--- src/components/layouts/AdminLayout.tsx
+++ src/components/layouts/AdminLayout.tsx
@@ -1,8 +1,9 @@
 import { Outlet } from "react-router-dom";
+import "../../styles/layouts/adminLayout.scss";
 
 export const AdminLayout = () => {
   return (
-    <div className="admin__layout ">
+    <div className="admin__layout">
       {/* // <div className="admin__layout surface-ground h-screen"> */}
       <Outlet />
     </div>
