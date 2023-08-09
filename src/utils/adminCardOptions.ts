8f22282d99a023d68f0339a7862f1602c450a65e
leo <leopoldo.miranda18@tectijuana.edu.mx>
2023-08-08 23:55:55 -0700

    add: home.scss

---
+++ src/utils/adminCardOptions.ts
@@ -0,0 +1,6 @@
+export const adminCardOptions = [
+  { title: "Users", icon: "pi-user" },
+  { title: "Categories", icon: "pi-th-large" },
+  { title: "Products", icon: "pi-table" },
+  { title: "Coupons", icon: "pi-ticket" },
+];
