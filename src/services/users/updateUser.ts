109ccaeff5ceb871e9465f971c6bea526918eafe
leo <leopoldo.miranda18@tectijuana.edu.mx>
2023-08-25 23:33:25 -0700

    fix: update user admin files

--- src/services/users/updateUser.ts
+++ src/services/users/updateUser.ts
@@ -1,19 +1,17 @@
-import axios from "axios"
+import axios from "axios";
 
-export const updateUser = async(id, user) => {
-  console.log('getUser ...', id);
+export const updateUser = async (id, user) => {
   try {
     const response = await axios({
       url: `http://127.0.0.1:5000/api/user/${id}`,
       data: user,
-      method: 'PUT',
-      header: {
-        'Content-Type': 'application/json'
-      }
-    })
-    return response
-
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    return response;
   } catch (error) {
-    console.log('Error... ', error)
+    console.log("Error... ", error);
   }
-}
+};
