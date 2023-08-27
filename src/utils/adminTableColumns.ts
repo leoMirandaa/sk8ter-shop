8105b397acb4851fee0b7fb71a1dfd729839c6cc
leo <leopoldo.miranda18@tectijuana.edu.mx>
2023-08-26 20:20:10 -0700

    fix: update admin table column

---
+++ src/utils/adminTableColumns.ts
@@ -0,0 +1,33 @@
+const categoryColumns = ["name", "status", "createdAt", "updatedAt"];
+
+const userColumns = [
+  "firstName",
+  "lastName",
+  "email",
+  "phone",
+  "street",
+  "country",
+  "state",
+  "role",
+  "status",
+  // "Password",
+  // "Actions",
+];
+
+const productColumns = [
+  "img",
+  "name",
+  "description",
+  "price",
+  "status",
+  "category",
+];
+
+const couponColumns = ["code", "discount"];
+
+export default {
+  categoryColumns,
+  userColumns,
+  productColumns,
+  couponColumns,
+};
