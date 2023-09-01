299a1dd6e965b493165f91c9e63be1e07fadf759
leo <leopoldo.miranda18@tectijuana.edu.mx>
2023-08-31 22:59:07 -0700

    feat: add get and update product service

--- src/services/products.service.ts
+++ src/services/products.service.ts
@@ -1,4 +1,6 @@
 import axios from "axios";
+import { Product } from "../interfaces";
+const url = import.meta.env.VITE_REACT_APP_URL2;
 
 const getProducts = async () => {
   try {
@@ -12,6 +14,49 @@ const getProducts = async () => {
     return response;
   } catch (error) {
     console.log("ERROR_GET_PRODUCTS ", error);
+  }
+};
+
+const getProduct = async (id: string) => {
+  console.log("ID: ", id);
+  try {
+    const response = await axios({
+      url: `${url}/products/${id}`,
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    return response;
+  } catch (error) {
+    console.log("ERROR_GET_PRODUCT: ", error);
+  }
+};
+
+const createProduct = async (product: Product) => {
+  console.log("ProductINService: ", product);
+  try {
+    let formData = new FormData();
+    formData.append("name", product.name);
+    formData.append("description", product.description);
+    formData.append("price", product.price);
+    formData.append("category", product.category._id);
+    formData.append("img", product.img);
+
+    console.log("formData: ", formData);
+
+    const response = await axios({
+      url: `${url}/products`,
+      method: "POST",
+      data: formData,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    return response;
+  } catch (error) {
+    console.log("ERROR_CREATE_PRODUCT");
+    return error.response;
   }
 };
 
@@ -32,4 +77,37 @@ const deleteProduct = async (id: string) => {
   }
 };
 
-export default { getProducts, deleteProduct };
+const updateProduct = async (id: string, product: any) => {
+  console.log({ id });
+  console.log({ product });
+  try {
+    let formData = new FormData();
+    formData.append("name", product.name);
+    formData.append("description", product.description);
+    formData.append("price", product.price);
+    formData.append("category", product.category._id);
+    formData.append("img", product.img);
+
+    console.log("formData: ", formData);
+    const response = await axios({
+      url: `${url}/products/${id}`,
+      method: "PUT",
+      data: formData,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    return response;
+  } catch (error) {
+    console.log("ERROR_UPDATE_PRODUCT");
+    return error.response;
+  }
+};
+
+export default {
+  getProducts,
+  getProduct,
+  createProduct,
+  deleteProduct,
+  updateProduct,
+};
