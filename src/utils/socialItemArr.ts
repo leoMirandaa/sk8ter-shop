8e0fcaf3d6443dff2c3adc509b8eb8e550e10166
leo <leopoldo.miranda18@tectijuana.edu.mx>
2023-09-27 23:51:29 -0700

    fix: socialItemArr.ts

--- src/utils/socialItemArr.ts
+++ src/utils/socialItemArr.ts
@@ -1,16 +1,12 @@
 export const socialItems = [
   {
-    name: "facebook",
-    icon: "pi pi-facebook",
+    name: "github",
+    icon: "pi pi-github",
     link: "",
   },
   {
     name: "instagram",
     icon: "pi pi-instagram",
-  },
-  {
-    name: "twitter",
-    icon: "pi pi-twitter",
   },
   {
     name: "youtube",
